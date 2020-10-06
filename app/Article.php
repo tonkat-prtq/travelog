0ae80057b87f76e1cbd064471e41944c0800e2f3
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-06 21:08:00 +0900

    :bird: #2 ArticleとUserの関連付け

--- app/Article.php
+++ app/Article.php
@@ -4,7 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class Article extends Model
 {
-    //
+
+    public function user(): BelongsTo // BelongsToという型で返ってくる宣言(それ以外だとTypeError発生)
+    {
+        return $this->belongsTo('App\User'); // $thisはArticleクラスのインスタンス自身
+    }
+
 }
