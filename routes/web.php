70ccbf1934b38c2a98885c388704f83d3b5d48be
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-14 17:09:48 +0900

    :bird: #12 Article#showでは、authミドルウェアを使用しないようルーティングを上書き

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ Auth::routes();
 Route::get('/', 'ArticleController@index')->name('articles.index');
 
 Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth');
+
+// 上書きして、authミドルウェアを外す
+Route::resource('/articles', 'ArticleController')->only(['show']);
