ee63b1428ea1e72f56e41bc9e6efae75bc24d6c8
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-14 17:31:44 +0900

    :bird: #12 Articleのポリシー作成

---
+++ app/Policies/ArticlePolicy.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Policies;
+
+use App\Article;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ArticlePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any articles.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+
+    // コントローラのindexと対応
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the article.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Article  $article
+     * @return mixed
+     */
+
+    // コントローラのshowと対応
+    public function view(User $user, Article $article)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create articles.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+
+    // コントローラのcreate, storeと対応
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the article.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Article  $article
+     * @return mixed
+     */
+
+    // コントローラのedit, updateと対応
+    public function update(User $user, Article $article)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the article.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Article  $article
+     * @return mixed
+     */
+
+    // コントローラのdestroyと対応
+    public function delete(User $user, Article $article)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the article.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Article  $article
+     * @return mixed
+     */
+    public function restore(User $user, Article $article)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the article.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Article  $article
+     * @return mixed
+     */
+    public function forceDelete(User $user, Article $article)
+    {
+        //
+    }
+}
