69509fbffb18f03d5e6251b11251228e1edbf9d5
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-14 16:25:53 +0900

    :bird: #10 start_dateとend_dateを入力するためのフォーム追加

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -3,7 +3,19 @@
   <label>タイトル</label>
   <input type="text" name="title" class="form-control" required value="{{ old('title') }}">
 </div>
+<div class="form-label">
+開始日
+</div>
+<div class="md-form">
+  <input type="date" name="start_date" class="form-control" required value="{{ old('start_date') }}">
+</div>
+<div class="form-label">
+終了日
+</div>
+<div class="md-form">
+  <input type="date" name="end_date" class="form-control" required value="{{ old('end_date') }}">
+</div>
 <div class="form-group">
   <label></label>
-  <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ old('body') }}</textarea>
+  <textarea name="content" required class="form-control" rows="16" placeholder="本文">{{ old('content') }}</textarea>
 </div>
