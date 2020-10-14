0db8969b38f473fe5a03290da016a2c8e0a1f3f3
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-14 17:06:55 +0900

    :bird: #12 Articleコントローラにdestroyアクション追加、記事の削除を実装

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -46,4 +46,10 @@ class ArticleController extends Controller
         return redirect()->route('articles.index');
     }
 
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return redirect()->route('articles.index');
+    }
+
 }
