d02dbadc7664111caae3d464908e811f981e10c2
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-15 17:43:55 +0900

    :bird: #17 Articleのフォームに画像投稿フォーム仮追加（うまく動いていない）

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -15,6 +15,10 @@
 <div class="md-form">
   <input type="date" name="end_date" class="form-control" required value="{{ $article->end_date ?? old('end_date') }}">
 </div>
+<div class="form-file">
+    <label for="photo">画像ファイル（複数可）:</label>
+    <input type="file" class="form-control" name="files[][photo]" multiple>
+</div>
 <div class="form-group">
   <label></label>
   <textarea name="content" required class="form-control" rows="16" placeholder="本文">{{ $article->content ?? old('content') }}</textarea>
