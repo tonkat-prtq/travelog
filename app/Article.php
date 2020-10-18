e8322816c591bf083e6041ca8bfc2a7fc28a8407
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-18 21:58:59 +0900

    :bird: #26 ArticleモデルにTagとの関連付けを多対多で設定

--- app/Article.php
+++ app/Article.php
@@ -56,4 +56,9 @@ class Article extends Model
         // countでその数を数えている
         return $this->likes->count();
     }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Tag')->withTimestamps();
+    }
 }
