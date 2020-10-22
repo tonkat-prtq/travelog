8f9c89ac6dda2c8f50cc359a691738866c17c734
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-23 07:45:01 +0900

    :bird: #29 Userコントローラにshowアクションメソッド追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function show(string $name)
+    {
+        // $nameにはルーティングの{name}に入った文字列が渡ってくる
+        // Userのnameカラムが、$nameと一致するユーザーを取ってくる
+        $user = User::where('name', $name)->first();
+
+        return view('users.show', [
+            'user' => $user,
+        ]);
+    }
 }
