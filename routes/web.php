0d996d5af090b415911211ea511e048a001198c5
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-23 07:34:07 +0900

    :bird: #29 ユーザーページ表示のルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,33 @@
 Auth::routes();
 
 // rootページにアクセスがあったら、ArticleControllerのIndexアクションの処理が走る
-Route::get('/', 'ArticleController@index')->name('articles.index');
+Route::get('/', 'ArticleController@index')
+  ->name('articles.index');
 
-Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth');
+Route::resource('/articles', 'ArticleController')
+  ->except(['index'])
+  ->middleware('auth');
 
 // 上書きして、authミドルウェアを外す
-Route::resource('/articles', 'ArticleController')->only(['show']);
+Route::resource('/articles', 'ArticleController')
+  ->only(['show']);
 
-Route::prefix('articles')->name('articles.')->group(function() {
-  Route::put('/{article}/like', 'ArticleController@like')->name('like')->middleware('auth');
-  Route::delete('/{article}/like', 'ArticleController@unlike')->name('unlike')->middleware('auth');
-});
+Route::prefix('articles')
+  ->name('articles.')
+  ->group(function() {
+    Route::put('/{article}/like', 'ArticleController@like')
+      ->name('like')
+      ->middleware('auth');
+    Route::delete('/{article}/like', 'ArticleController@unlike')
+      ->name('unlike')
+      ->middleware('auth');
+  });
 
 Route::get('/tags/{name}', 'TagController@show')->name('tags.show');
+
+Route::prefix('users')
+  ->name('users.')
+  ->group(function() {
+    Route::get('/{name}','UserController@show')
+      ->name('show');
+  });
