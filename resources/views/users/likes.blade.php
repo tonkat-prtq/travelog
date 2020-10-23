ea10b73da4115c28f925d9871e1cd0b8ad74e3c8
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-23 23:15:38 +0900

    :grapes: #29 ユーザー情報を切り出して共通化

--- resources/views/users/likes.blade.php
+++ resources/views/users/likes.blade.php
@@ -1,6 +1,6 @@
 @extends('app')
 
-@section('title', $user->name)
+@section('title', $user->name . 'のいいねした記事')
 
 @section('content')
   @include('nav')
@@ -8,14 +8,14 @@
     @include('users.user')
     <ul class="nav nav-tabs nav-justified mt-3">
       <li class="nav-item">
-        <a class="nav-link text-muted active"
+        <a class="nav-link text-muted"
           href="{{ route('users.show', ['name' => $user->name]) }}">
           記事
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link text-muted"
-          href="{{ route('users.likes', ['name' => $user->name ]) }} ">
+        <a class="nav-link text-muted active"
+          href="{{ route('users.likes', ['name' => $user->name]) }}">
           いいね
         </a>
       </li>
