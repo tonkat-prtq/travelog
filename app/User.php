84c02be15b5281944a0774c06becf71199591246
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-23 22:39:35 +0900

    :bird: #29 フォローしている、あるいはこれからフォローするユーザーのモデルにアクセスするためのリレーションメソッド追加

--- app/User.php
+++ app/User.php
@@ -46,9 +46,16 @@ class User extends Authenticatable
         $this->notify(new PasswordResetNotification($token, new BareMail()));
     }
 
+    // 自分をフォローしているユーザーのモデルにアクセスするためのリレーションメソッド
     public function followers(): BelongsToMany
     {
         return $this->belongsToMany('App\User', 'follows', 'followee_id', 'follower_id')->withTimestamps();
+    }
+
+    // フォローしているユーザー、あるいはこれからフォローするユーザーのモデルにアクセスするためのリレーションメソッド
+    public function followings(): BelongsToMany
+    {
+        return $this->belongsToMany('App\User', 'follows', 'follower_id', 'followee_id')->withTimestamps();
     }
 
     public function isFollowedBy(?User $user): bool
