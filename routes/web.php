65d6dd80d258af4ed2d1384492d245a8d94d043a
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-24 22:59:51 +0900

    :bird: #31 Googleでユーザーログイン・登録するためのルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ Route::prefix('login')
   ->group(function() {
     Route::get('/{provider}', 'Auth\LoginController@redirectToProvider')
       ->name('{provider}');
+    Route::get('/{provider}/callback', 'Auth\LoginController@handleProviderCallback')
+      ->name('{provider}.callback');
   });
 
 // rootページにアクセスがあったら、ArticleControllerのIndexアクションの処理が走る
