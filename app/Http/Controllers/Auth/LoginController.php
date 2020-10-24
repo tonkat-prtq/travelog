8bd8cf06dc2fd12d67da5a18a6459deadd68ba26
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-24 22:57:35 +0900

    :grapes: #31 Socialiteのuse設定がなかったので追記

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+use Laravel\Socialite\Facades\Socialite;
+
 class LoginController extends Controller
 {
     /*
