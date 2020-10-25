b48e5ea4a0bb45431c415a9e48e3e37006f68af9
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-25 16:50:13 +0900

    :grapes: #34 記事全件と一緒に紐付いているuser, likes, tagsのデータも持ってくるように

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -21,7 +21,11 @@ class ArticleController extends Controller
     public function index()
     {
         $articles = Article::all()->sortByDesc('created_at')
-            ->load('user');
+            ->load([
+                'user',
+                'likes',
+                'tags'
+            ]);
 
         return view('articles.index', ['articles' => $articles]);
     }
