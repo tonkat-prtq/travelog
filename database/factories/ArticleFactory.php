60b1b4d7570278de4616ba049d1e096584bc107a
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-10-27 23:26:12 +0900

    :bird: #37 いいね機能のテスト

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Article;
+
+use App\User;
+
+use Carbon\Carbon;
+use Faker\Generator as Faker;
+
+$factory->define(Article::class, function (Faker $faker) {
+    return [
+        'title' => $faker->text(50),
+        'content' => $faker->text(500),
+        'start_date' => Carbon::now(),
+        'end_date' => Carbon::today('+1 day'),
+        'user_id' => function() {
+            return factory(User::class);
+        }
+    ];
+});
