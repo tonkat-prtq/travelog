be55f7345ee6d4c25a055ef23eb6d0b8bab7b754
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-11-01 15:17:12 +0900

    :bird: #49 検索用コントローラ作成

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Article;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function search(Request $request)
+    {
+        $keyword = $request->input;
+        $query = Article::query();
+
+        if (!empty($keyword)) {
+            $query->where('content', 'LIKE', "%{$keyword}%");
+        }
+
+        $articles = $query->get();
+        return view('search.index', ['articles' => $articles]);
+    }
+
+    public function index(Request $request)
+    {
+        if (isset($article)) {
+            return view('search.index', ['articles' => $articles]);
+        } else {
+            return redirect('/');
+        }
+    }
+}
