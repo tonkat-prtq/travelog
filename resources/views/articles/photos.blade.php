69ea3c597d2382bd9a89b270341833476d3bc12e
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-11-03 19:57:25 +0900

    :grapes: #65 storage_keyの中をそのまま読むことで、s3にアップロードした画像を表示させる

--- resources/views/articles/photos.blade.php
+++ resources/views/articles/photos.blade.php
@@ -1,5 +1,5 @@
 <div class="photos">
   @foreach($article->photos as $photo)
-  <img src="{{asset("storage/{$photo->storage_key}") }}" class="img-fluid" alt="Responsive Image"> 
+  <img src="{{ $photo->storage_key }}" class="img-fluid" alt="Responsive Image"> 
   @endforeach
 </div>
