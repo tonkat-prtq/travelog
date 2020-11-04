2a003da7b8b5bb5832b127f59958d75a8af62593
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-11-04 23:47:39 +0900

    :bird: #52 カルーセルを自動でスライドさせないためのJavaScriptを書く

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,7 +1,14 @@
 @extends('app')
 
-@section('title', '記事一覧')
+@section('addJS')
+  <script>
+  window.onload = function(){
+    $("#carousel-example-1z").carousel('pause');
+  };
+  </script>
+@endsection
 
+@section('title', '記事一覧')
 @section('content')
   @include('nav')
   <div class="container">
