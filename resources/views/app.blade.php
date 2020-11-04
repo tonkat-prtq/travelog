0805a614847608d81dac4254849c1ff20998c82c
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-11-04 23:46:58 +0900

    :bird: #52 @yield('addCSS')を作って、各bladeで使うCSSを書きやすいように

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,6 +13,7 @@
   <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
   <!-- Material Design Bootstrap -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/css/mdb.min.css" rel="stylesheet">
+  @yield('addCSS')
 </head>
 
 <body>
@@ -29,6 +30,7 @@
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
   <!-- MDB core JavaScript -->
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/js/mdb.min.js"></script>
+  @yield('addJS')
 </body>
 
 </html>
