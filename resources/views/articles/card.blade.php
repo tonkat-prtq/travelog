f9dc7ca78658bc1a6e8dfd44272495e52c238182
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-11-06 23:10:32 +0900

    :grapes: #70 写真がないときはカルーセルのボタンが表示されないように。カードヘッダー的なものを追加。

--- resources/views/articles/card.blade.php
+++ resources/views/articles/card.blade.php
@@ -1,6 +1,4 @@
 <div class="card mt-3">
-    {{-- 画像表示専用のビューを作って呼び出している --}}
-    @include('articles.photos')
   <div class="card-body d-flex flex-row">
     <a href="{{ route('users.show', ['name' => $article->user->name]) }}" class="text-dark">
     <i class="fas fa-user-ninja fa-3x mr-1"></i>
@@ -13,54 +11,59 @@
       </div>
       <div class="font-weight-lighter">{{ $article->created_at->format('Y/m/d H:i') }}</div>
     </div>
-
-  @if( Auth::id() === $article->user_id )
-    <!-- dropdown -->
-      <div class="ml-auto card-text">
-        <div class="dropdown">
-          <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-ellipsis-v"></i>
-          </a>
-          <div class="dropdown-menu dropdown-menu-right">
-            <a class="dropdown-item" href="{{ route("articles.edit", ['article' => $article]) }}">
-              <i class="fas fa-pen mr-1"></i>記事を更新する
-            </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item text-danger" data-toggle="modal" data-target="#modal-delete-{{ $article->id }}">
-              <i class="fas fa-trash-alt mr-1"></i>記事を削除する
-            </a>
-          </div>
-        </div>
-      </div>
+      <div class="ml-auto">
+      @if( Auth::id() === $article->user_id )
       <!-- dropdown -->
-
-      <!-- modal -->
-      <div id="modal-delete-{{ $article->id }}" class="modal fade" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
-                <span aria-hidden="true">&times;</span>
-              </button>
+        <div class="ml-auto card-text">
+          <div class="dropdown">
+            <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i class="fas fa-ellipsis-v"></i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right">
+              <a class="dropdown-item" href="{{ route("articles.edit", ['article' => $article]) }}">
+                <i class="fas fa-pen mr-1"></i>記事を更新する
+              </a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item text-danger" data-toggle="modal" data-target="#modal-delete-{{ $article->id }}">
+                <i class="fas fa-trash-alt mr-1"></i>記事を削除する
+              </a>
             </div>
-            <form method="POST" action="{{ route('articles.destroy', ['article' => $article]) }}">
-              @csrf
-              @method('DELETE')
-              <div class="modal-body">
-                {{ $article->title }}を削除します。よろしいですか？
-              </div>
-              <div class="modal-footer justify-content-between">
-                <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
-                <button type="submit" class="btn btn-danger">削除する</button>
-              </div>
-            </form>
           </div>
         </div>
-      </div>
-      <!-- modal -->
-    @endif
+        <!-- dropdown -->
 
+        <!-- modal -->
+        <div id="modal-delete-{{ $article->id }}" class="modal fade" tabindex="-1" role="dialog">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <form method="POST" action="{{ route('articles.destroy', ['article' => $article]) }}">
+                @csrf
+                @method('DELETE')
+                <div class="modal-body">
+                  {{ $article->title }}を削除します。よろしいですか？
+                </div>
+                <div class="modal-footer justify-content-between">
+                  <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
+                  <button type="submit" class="btn btn-danger">削除する</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <!-- modal -->
+      @endif
+    </div>
   </div>
+
+    {{-- 画像表示専用のビューを作って呼び出している --}}
+    @if (($article->photos->count() > 0))
+      @include('articles.photos')
+    @endif
   <div class="card-body pt-0">
 
     <h3 class="h4 card-title">
