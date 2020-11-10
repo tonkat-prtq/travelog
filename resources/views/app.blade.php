a57802434aa92982937aeb4a9351676294dde04f
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-11-10 21:51:08 +0900

    :grapes: #20 MDBootstrapのcdn読み込みを削除

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,10 +9,7 @@
   </title>
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-  <!-- Bootstrap core CSS -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Material Design Bootstrap -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/css/mdb.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   @yield('addCSS')
 </head>
 
@@ -28,8 +25,6 @@
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
   <!-- Bootstrap core JavaScript -->
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
-  <!-- MDB core JavaScript -->
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/js/mdb.min.js"></script>
   @yield('addJS')
 </body>
 
