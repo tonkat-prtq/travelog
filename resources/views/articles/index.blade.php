db908c01a574c12b5d2f9850b017bb17cb8f35b5
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-11-13 21:53:09 +0900

    :bird: #20 上に戻るボタン設置

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -15,5 +15,38 @@
     @foreach($articles as $article)
       @include('articles.card')
     @endforeach
+
+    <div class="pagetop">
+      <p class="mt-2 float-right"><a href="#top"><i class="fas fa-arrow-circle-up fa-4x text-first-color ml-auto"></i></a></p>
+    </div>
+  </div>
+@section('addJS')
+<script type="text/javascript">
+    $(function(){
+        var amount = 200; //スクロール量（px）
+        $('.pagetop').hide();
+        $(window).scroll(function(){
+            var scrollPoint = $(this).scrollTop();
+            (scrollPoint > amount)?$('.pagetop').fadeIn():(scrollPoint < amount)?$('.pagetop').fadeOut():$('.pagetop').show();
+        });
+    });
+</script>
+@endsection
+
+@section('addCSS')
+  <style>
+
+    .container {
+      position: relative;
+    }
+
+    .pagetop{
+      position:fixed;
+      bottom:5px;
+      right:5px;
+    }
+
+  </style>
+@endsection
   </div>
 @endsection
