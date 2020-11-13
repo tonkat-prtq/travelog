8f5944daf2be40e164ff1a2d508ed7479e862e19
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-11-13 22:28:48 +0900

    :bird: #20 ログイン画面でもnavbarを表示するように。ログイン画面のフォームのデザイン

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,11 +2,16 @@
 
 @section('title', 'ログイン')
 
+@include('nav')
+
+@include('error_card_list')
+
+<link rel="stylesheet" href="{{ asset('css/card.css') }}">
+
 @section('content')
   <div class="container">
     <div class="row">
       <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
-        <h1 class="text-center"><a class="text-dark" href="/">memo</a></h1>
         <div class="card mt-3">
           <div class="card-body text-center">
             <h2 class="h3 card-title text-center mt-2">ログイン</h2>
@@ -14,36 +19,34 @@
               <i class="fab fa-google mr-1"></i>Googleでログイン
             </a>
 
-            @include('error_card_list')
 
             <div class="card-text">
               <form method="POST" action="{{ route('login') }}">
                 @csrf
 
-                <div class="md-form">
+                <div class="form-group mt-4">
                   <label for="email">メールアドレス</label>
-                  <input class="form-control" type="text" id="email" name="email" required value="{{ old('email') }}">
+                  <input class="form-control" type="text" id="email" name="email" placeholder="example@com" required value="{{ old('email') }}">
                 </div>
 
-                <div class="md-form">
+                <div class="form-group">
                   <label for="password">パスワード</label>
                   <input class="form-control" type="password" id="password" name="password" required>
                 </div>
 
                 <input type="hidden" name="remember" id="remember" value="on">
 
-                <div class="text-left">
-                  <a href="{{ route('password.request') }}" class="card-text">パスワードを忘れた方</a>
-                </div>
-
-                <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ログイン</button>
+                <button class="btn-first-color btn-block mt-2 mb-2 shadow-none" type="submit">ログイン</button>
 
               </form>
 
               <div class="mt-0">
-                <a href="{{ route('register') }}" class="card-text">ユーザー登録はこちら</a>
+                <a href="{{ route('register') }}" class="card-text btn-block btn-second-color text-decoration-none mt-1 mb-1 pt-1 pb-1">ユーザー登録はこちら</a>
               </div>
 
+              <div class="small mt-2">
+                <a href="{{ route('password.request') }}" class="card-text">パスワードを忘れた方</a>
+              </div>
             </div>
           </div>
         </div>
