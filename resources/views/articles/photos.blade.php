97077dbb7a19d678e0681c3d0204efac5e299b58
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-11-14 23:45:04 +0900

    :grapes: #72 下の記事でカルーセルのボタンが効かない問題を解決 close #72

--- resources/views/articles/photos.blade.php
+++ resources/views/articles/photos.blade.php
@@ -1,11 +1,11 @@
 <!--Carousel Wrapper-->
-<div id="carousel-example-1z" class="carousel slide carousel-fade mt-2" data-ride="carousel" data-interval="false">
+<div id="carousel-{{ $article->id }}" class="carousel slide carousel-fade mt-2" data-ride="carousel" data-interval="false">
   <ol class="carousel-indicators">
     @foreach($article->photos as $photo)
       @if($loop->first)
-        <li data-target="#carousel-example-1z" data-slide-to="{{$loop->index}}" class="active"></li>
+        <li data-target="#carousel-{{ $article->id }}" data-slide-to="{{$loop->index}}" class="active"></li>
       @else
-        <li data-target="#carousel-example-1z" data-slide-to="{{$loop->index}}"></li>
+        <li data-target="#carousel-{{ $article->id }}" data-slide-to="{{$loop->index}}"></li>
       @endif
     @endforeach
   </ol>
@@ -20,13 +20,13 @@
       </div>
     @endforeach
   </div>
-  <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#carousel-{{ $article->id }}" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true">
       <i class="fas fa-chevron-left text-black-50 fa-3x"></i>
     </span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#carousel-{{ $article->id }}" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true">
       <i class="fas fa-chevron-right text-black-50 fa-3x"></i>
     </span>
