e34e887f578bd666e0bb305fe5bcfc434b0b49dc
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-11-14 15:25:26 +0900

    :grapes: #20 パスワード再設定のためにメールアドレスを入力する画面のデザイン

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -2,11 +2,16 @@
 
 @section('title', 'パスワード再設定')
 
+@include('nav')
+
+@include('error_card_list')
+
+<link rel="stylesheet" href="{{ asset('css/card.css') }}">
+
 @section('content')
   <div class="container">
     <div class="row">
       <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
-        <h1 class="text-center"><a class="text-dark" href="/">memo</a></h1>
         <div class="card mt-3">
           <div class="card-body text-center">
             <h2 class="h3 card-title text-center mt-2">パスワード再設定</h2>
@@ -28,7 +33,7 @@
                   <input class="form-control" type="text" id="email" name="email" required>
                 </div>
 
-                <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">メール送信</button>
+                <button class="btn-third-color btn-block mt-4 mb-2" type="submit">メール送信</button>
 
               </form>
 
