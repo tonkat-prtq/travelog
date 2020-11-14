0ceb73e7bf2efb4e29fbef60382c872f4c349fa4
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-11-14 23:54:34 +0900

    :grapes: #20 navbarの色を濃いめのグリーンに変更

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand bg-third-color justify-content-center">
+<nav class="navbar navbar-expand bg-second-color justify-content-center">
 
-  <a class="navbar-brand ml-auto mr-auto text-black-50 text-decoration-none" href="/"><i class="fas fa-camera-retro mr-1"></i>Travelog</a>
+  <a class="navbar-brand ml-auto mr-auto text-white text-decoration-none" href="/"><i class="fas fa-camera-retro mr-1"></i>Travelog</a>
 
   <div class="navbar-nav ml-auto mr-auto vertical-align-center">
     <li class="nav-item">
@@ -15,36 +15,36 @@
 
     @guest 
     <li class="nav-item">
-      <a class="nav-link text-muted text-decoration-none" href="{{ route('register') }}">ユーザー登録</a>
+      <a class="nav-link text-white text-decoration-none" href="{{ route('register') }}">ユーザー登録</a>
     </li>
     @endguest 
 
     @guest 
     <li class="nav-item">
-      <a class="nav-link text-muted text-decoration-none" href="{{ route('login') }}">ログイン</a>
+      <a class="nav-link text-white text-decoration-none" href="{{ route('login') }}">ログイン</a>
     </li>
     @endguest 
       
     @auth 
     <li class="nav-item">
-      <a class="nav-link text-muted text-decoration-none" href="{{ route('articles.create') }}"><i class="fas fa-pen mr-1"></i>投稿する</a>
+      <a class="nav-link text-white text-decoration-none" href="{{ route('articles.create') }}"><i class="fas fa-pen mr-1"></i>投稿する</a>
     </li>
     @endauth
     
     @auth 
     <!-- Dropdown -->
     <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle text-muted text-decoration-none" id="navbarDropdownMenuLink" data-toggle="dropdown"
+      <a class="nav-link dropdown-toggle text-white text-decoration-none" id="navbarDropdownMenuLink" data-toggle="dropdown"
         aria-haspopup="true" aria-expanded="false">
-        <i class="fas fa-user-circle text-muted text-decoration-none"></i>
+        <i class="fas fa-user-circle text-white text-decoration-none"></i>
       </a>
       <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
-        <button class="dropdown-item text-muted text-decoration-none" type="button"
+        <button class="dropdown-item text-decoration-none mypage-button" type="button"
                 onclick="location.href='{{ route("users.show", ["name" => Auth::user()->name]) }}'">
           マイページ
         </button>
         <div class="dropdown-divider"></div>
-        <button form="logout-button" class="dropdown-item" type="submit">
+        <button form="logout-button" class="dropdown-item logout-button" type="submit">
           ログアウト
         </button>
       </div>
