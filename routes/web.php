e6ef69cda4a0e080e6e9e21454b2738975e2b97b
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-11-25 22:02:55 +0900

    :bird: #93 エラーページ表示用のルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -3,71 +3,81 @@
 Auth::routes();
 
 Route::prefix('login')
-  ->name('login.')
-  ->group(function() {
-    Route::get('/{provider}', 'Auth\LoginController@redirectToProvider')
-      ->name('{provider}');
-    Route::get('/{provider}/callback', 'Auth\LoginController@handleProviderCallback')
-      ->name('{provider}.callback');
-  });
+    ->name('login.')
+    ->group(function () {
+        Route::get(
+            '/{provider}',
+            'Auth\LoginController@redirectToProvider',
+        )->name('{provider}');
+        Route::get(
+            '/{provider}/callback',
+            'Auth\LoginController@handleProviderCallback',
+        )->name('{provider}.callback');
+    });
 
 Route::prefix('register')
-  ->name('register.')
-  ->group(function() {
-    Route::get('/{provider}', 'Auth\RegisterController@showProviderUserRegistrationForm')
-      ->name('{provider}');
-    Route::post('/{provider}', 'Auth\RegisterController@registerProviderUser')
-      ->name('{provider}');
-});
+    ->name('register.')
+    ->group(function () {
+        Route::get(
+            '/{provider}',
+            'Auth\RegisterController@showProviderUserRegistrationForm',
+        )->name('{provider}');
+        Route::post(
+            '/{provider}',
+            'Auth\RegisterController@registerProviderUser',
+        )->name('{provider}');
+    });
 
 // rootページにアクセスがあったら、ArticleControllerのIndexアクションの処理が走る
-Route::get('/', 'ArticleController@index')
-  ->name('articles.index');
+Route::get('/', 'ArticleController@index')->name('articles.index');
 
 Route::resource('/articles', 'ArticleController')
-  ->except(['index'])
-  ->middleware('auth');
+    ->except(['index'])
+    ->middleware('auth');
 
 // 上書きして、authミドルウェアを外す
-Route::resource('/articles', 'ArticleController')
-  ->only(['show']);
+Route::resource('/articles', 'ArticleController')->only(['show']);
 
 Route::prefix('articles')
-  ->name('articles.')
-  ->group(function() {
-    Route::put('/{article}/like', 'ArticleController@like')
-      ->name('like')
-      ->middleware('auth');
-    Route::delete('/{article}/like', 'ArticleController@unlike')
-      ->name('unlike')
-      ->middleware('auth');
-  });
+    ->name('articles.')
+    ->group(function () {
+        Route::put('/{article}/like', 'ArticleController@like')
+            ->name('like')
+            ->middleware('auth');
+        Route::delete('/{article}/like', 'ArticleController@unlike')
+            ->name('unlike')
+            ->middleware('auth');
+    });
 
 Route::get('/tags/{name}', 'TagController@show')->name('tags.show');
 
 Route::prefix('users')
-  ->name('users.')
-  ->group(function() {
-    Route::get('/{name}','UserController@show')
-      ->name('show');
-    Route::get('/{name}/likes', 'UserController@likes')
-      ->name('likes');
-    Route::get('/{name}/followings', 'UserController@followings')
-      ->name('followings');
-    Route::get('/{name}/followers', 'UserController@followers')
-      ->name('followers');
-    Route::middleware('auth')
-      ->group(function() {
-        Route::put('/{name}/follow', 'UserController@follow')
-          ->name('follow');
-        Route::delete('/{name}/follow', 'UserController@unfollow')
-          ->name('unfollow');
-      });
-  });
+    ->name('users.')
+    ->group(function () {
+        Route::get('/{name}', 'UserController@show')->name('show');
+        Route::get('/{name}/likes', 'UserController@likes')->name('likes');
+        Route::get('/{name}/followings', 'UserController@followings')->name(
+            'followings',
+        );
+        Route::get('/{name}/followers', 'UserController@followers')->name(
+            'followers',
+        );
+        Route::middleware('auth')->group(function () {
+            Route::put('/{name}/follow', 'UserController@follow')->name(
+                'follow',
+            );
+            Route::delete('/{name}/follow', 'UserController@unfollow')->name(
+                'unfollow',
+            );
+        });
+    });
 
-Route::get('/search','SearchController@index')
-  ->name('index');
+Route::get('/search', 'SearchController@index')->name('index');
 Route::post('/search', 'SearchController@search');
 
-Route::get('/top', 'TopController@index')
-  ->name('index');
+Route::get('/top', 'TopController@index')->name('index');
+
+// HTTPステータスコードを引数に、該当するエラーページを表示させる
+Route::get('error/{code}', function ($code) {
+    abort($code);
+});
