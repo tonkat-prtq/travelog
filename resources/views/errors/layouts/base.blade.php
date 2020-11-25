b928bfb2785366390db45b1bef6c7c39a4a5e37a
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-11-25 22:15:19 +0900

    :grapes: #93 margin修正

--- resources/views/errors/layouts/base.blade.php
+++ resources/views/errors/layouts/base.blade.php
@@ -7,9 +7,9 @@
 @section('content')
 <div class="error-wrap">
   <section>
-    <h1 class="text-center mt-3 mb-3">@yield('title')</h1>
-    <p class="error-message text-center h4 font-weight-light">@yield('message')</p>
-    <p class="error-detail text-center h5 font-weight-light mb-2">@yield('detail')</p>
+    <h1 class="text-center mt-3 mb-5">@yield('title')</h1>
+    <p class="error-message text-center h4 font-weight-light mb-3">@yield('message')</p>
+    <p class="error-detail text-center h5 font-weight-light mb-4">@yield('detail')</p>
     @yield('link')
   </section>
 </div>
