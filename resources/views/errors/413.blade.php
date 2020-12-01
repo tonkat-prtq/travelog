01823bcfbe9957444d126b8efd562cff63123e72
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-12-01 10:09:46 +0900

    :grapes: #96 413エラーのページを中身作成、post_too_largeを削除

--- resources/views/errors/413.blade.php
+++ resources/views/errors/413.blade.php
@@ -1,1 +1,13 @@
-test
+test@extends('errors.layouts.base')
+
+@section('title', 'Filesize Over')
+
+@section('message', 'アップロードしようとしている画像ファイルの合計サイズが大きすぎます')
+
+@section('detail', 'アップロードする画像のサイズを減らして、再投稿してください')
+
+@section('link')
+  <p class="text-center">
+    <a class="text-second-color" href="{{env('APP_URL')}}">トップページへ戻る</a>
+  </p>
+@endsection
