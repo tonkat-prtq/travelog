1afc5832fb81bb5ef7ea8207c22214b56357fe45
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-12-06 23:53:48 +0900

    :grapes: #108 faviconを追加……もうまく表示されない

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,11 @@
   <title>
     @yield('title')
   </title>
+
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/apple-touch-icon.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon-16x16.png') }}">
+  <link rel="manifest" href="{{ asset('images/site.webmanifest') }}">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
   <!-- Googleフォント -->
