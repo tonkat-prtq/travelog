a5338ce628b880b4c107e9a4bafcba588cd81103
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-12-08 21:10:33 +0900

    :grapes: #110 SearchControllerにphpdocを書く

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -6,6 +6,18 @@ use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
+    /**
+     * 全ての記事の中から、キーワードであいまい検索
+     * 検索対象は
+     *  Article->title,
+     *  Article->content,
+     *  tags->nameの3つ
+     *
+     * @param \Illuminate\Http\Request $request
+     * @var string $keyword
+     *
+     * @return \Illuminate\View\View|\Illuminate\Contracts\View\Factory
+     */
     public function search(Request $request)
     {
         $keyword = $request->input;
@@ -13,11 +25,11 @@ class SearchController extends Controller
         // withでeager loadしている
         $articles = Article::with(['user', 'likes', 'tags', 'photos'])
             // もし検索キーワードはnullでなかったら以下のクエリを発行する
-            ->when(!is_null($keyword), function($query) use ($keyword) {
+            ->when(!is_null($keyword), function ($query) use ($keyword) {
                 $query
                     ->where('content', 'LIKE', "%{$keyword}%")
                     ->orWhere('title', 'LIKE', "%{$keyword}%")
-                    ->orWhereHas('tags',function($query) use($keyword) {
+                    ->orWhereHas('tags', function ($query) use ($keyword) {
                         $query->where('name', 'LIKE', "%{$keyword}%");
                     });
             })
@@ -25,8 +37,14 @@ class SearchController extends Controller
             ->orderBy('created_at', 'desc')
             ->get();
         return view('search.index', ['articles' => $articles]);
-        }
+    }
 
+    /**
+     * 検索した結果、1件でもあればその結果を表示し、なければrootページ(Article#index)にリダイレクトする
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\View\View|\Illuminate\Contracts\View\Factory | \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
+     */
     public function index(Request $request)
     {
         if (isset($article)) {
